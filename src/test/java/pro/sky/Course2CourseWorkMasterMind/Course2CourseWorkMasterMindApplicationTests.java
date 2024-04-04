3330e004da5b9d2d2f3236f6f45b4b9feb5d170d
kolia <koliamba_85@mail.ru>
2024-04-05 00:57:02 +0800

    Исходный коммит

---
+++ src/test/java/pro/sky/Course2CourseWorkMasterMind/Course2CourseWorkMasterMindApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Course2CourseWorkMasterMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2CourseWorkMasterMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
